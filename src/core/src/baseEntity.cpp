aea38e4e615275e993a2be78cc9d3e4f34c00726
Curovka <161213278+Curovka@users.noreply.github.com>
2025-09-23 16:41:09 +0300

    Update baseEntity.cpp
    create generateUUID

--- src/core/src/baseEntity.cpp
+++ src/core/src/baseEntity.cpp
@@ -2,11 +2,16 @@
 
 #include "../include/baseEntity.h"
 #include <iostream>
+#include "uuid.cpp"
 
 BaseEntity::BaseEntity() {
     id = generateUUID();
     createdAt = std::chrono::system_clock::now();
     updateAt = createdAt;
+}
+
+std::string BaseEntity::generateUUID() {
+    return uuid::generateUUIDv4();
 }
 
 void BaseEntity::updateTimestamp() {
