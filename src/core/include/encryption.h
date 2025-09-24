fdefd4d29f6c4b141c487e5d169c8c3958f363cd
Curovka <161213278+Curovka@users.noreply.github.com>
2025-09-24 10:54:49 +0300

    Update encryption.h

--- src/core/include/encryption.h
+++ src/core/include/encryption.h
@@ -15,7 +15,29 @@ private:
     std::vector<unsigned char> HMACSHA256(const std::vector<unsigned char>& key, const std::vector<unsigned char>& message);
     std::vector<unsigned char> SHA256(const std::vector<unsigned char>& input);
 
-    // AES реализация в будущем
+    // Константы AES
+    static const size_t BLOCK_SIZE = 16;
+    static const size_t KEY_SIZE = 32;
+    static const size_t IV_SIZE = 16;
+    static const size_t NUM_ROUNDS = 14;
+    uint32_t round_keys[60];
+
+    // AES реализация
+    void AESEncrypt(const std::vector<unsigned char>& plaintext, const std::vector<unsigned char>& iv, std::vector<unsigned char>& ciphertext);
+    void AESDecrypt(const std::vector<unsigned char>& ciphertext, const std::vector<unsigned char>& iv, std::vector<unsigned char>& plaintext);
+    void AESEncryptBlock(const unsigned char* input, unsigned char* output);
+    void AESDecryptBlock(const unsigned char* input, unsigned char* output);
+
+    //вспомогательные функции для шифрования и дешифрования
+    void subBytes(unsigned char state[BLOCK_SIZE]);
+    void invSubBytes(unsigned char state[BLOCK_SIZE]);
+    void shiftRows(unsigned char state[BLOCK_SIZE]);
+    void invShiftRows(unsigned char state[BLOCK_SIZE]);
+    void mixColumns(unsigned char state[BLOCK_SIZE]);
+    void invMixColumns(unsigned char state[BLOCK_SIZE]);
+    unsigned char multiply(unsigned char a, unsigned char b);
+    void addRoundKey(unsigned char state[BLOCK_SIZE], int round);
+
     void keyExpansion();
     void rotWord(unsigned char word[4]);
     void subWord(unsigned char word[4]);
