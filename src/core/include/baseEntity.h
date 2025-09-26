d3d2cc27241d2c7c1fa2d45a37744939aaee5a77
Curovka <161213278+Curovka@users.noreply.github.com>
2025-09-26 17:40:46 +0300

    Update baseEntity.h

--- src/core/include/baseEntity.h
+++ src/core/include/baseEntity.h
@@ -1,27 +1,26 @@
-// base_entity.h
+// baseEntity.h
+#ifndef BASE_ENTITY_H
+#define BASE_ENTITY_H
 
-#include <string>
-#include <chrono>
-
-#ifndef BASEENTITY.H
-#define BASEENTITY.H
+#include "resources.h"
 
 class BaseEntity {
 protected:
     std::string id;
     std::chrono::system_clock::time_point createdAt;
-    std::chrono::system_clock::time_point updateAt;
+    std::chrono::system_clock::time_point updatedAt;
 
 public:
     BaseEntity();
+    virtual ~BaseEntity() = default;
 
     std::string generateUUID();
+    void updateTimestamp();
 
-    //getters
-    std::string getId() const { return id;}
+    // getters
+    std::string getId() const { return id; }
     std::chrono::system_clock::time_point getCreatedAt() const { return createdAt; }
-    std::chrono::system_clock::time_point getUpdatedAt() const { return updateAt; }
+    std::chrono::system_clock::time_point getUpdatedAt() const { return updatedAt; }
 };
 
-
-#endif
+#endif // BASE_ENTITY_H
