4329726d95c7e5d617b0c57ae87952d08c0b9578
Curovka <161213278+Curovka@users.noreply.github.com>
2025-09-26 17:42:16 +0300

    Update uuid.cpp

--- src/core/src/uuid.cpp
+++ src/core/src/uuid.cpp
@@ -10,35 +10,23 @@
 //
 //
 
-#include <random>
-#include <sstream>
-#include <array>
-#include <iomanip>
-#include <iostream>
+#include "resources.h"
 
 namespace uuid {
     std::string generateUUIDv4() {
         std::array<uint8_t, 16> bytes;
-        std::random_device rd; //для "безопасных" случайных чисел
+        std::random_device rd;
+        std::uniform_int_distribution<uint8_t> dis(0, 255);
         
-        //генерация 128 бит случайности и создание массива байтов
-
         for (size_t i = 0; i < 16; ++i) {
-            if (i % sizeof(std::random_device::result_type) == 0) {
-                auto random_value = rd();
-                
-                // Копирование random_value побайтово
-                for (size_t j = 0; j < sizeof(random_value) && i + j < 16; ++j) {
-                    bytes[i + j] = static_cast<uint8_t>((random_value >> (j * 8)) & 0xFF);
-                }
-            }
+            bytes[i] = dis(rd);
         }
         
-        //устанавка версию 4 и вариант 2
+        // Установка версии 4 и варианта 2
         bytes[6] = (bytes[6] & 0x0F) | 0x40;
         bytes[8] = (bytes[8] & 0x3F) | 0x80;
         
-        //преобразование в строку
+        // Преобразование в строку
         std::stringstream ss;
         ss << std::hex << std::setfill('0');
         
